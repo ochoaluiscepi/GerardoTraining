aaf472ff680fe52da071b6df206617a879ca0cc2
ochoaluiscepi <37229115+ochoaluiscepi@users.noreply.github.com>
2024-04-23 21:18:18 -0600

    Update Startup.cs

--- BulkyBook/Startup.cs
+++ BulkyBook/Startup.cs
@@ -26,7 +26,7 @@ namespace BulkyBook
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ApplicationDbContext>(options =>
@@ -37,9 +37,7 @@ namespace BulkyBook
             services.AddScoped<IUnitOfWork,UnitOfWork>();
             services.AddControllersWithViews().AddRazorRuntimeCompilation();
             services.AddRazorPages();
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())

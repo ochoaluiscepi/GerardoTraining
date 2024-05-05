40b6eaf5a132dcc7aee61419f9de1bc81ac61da1
ochoaluiscepi <37229115+ochoaluiscepi@users.noreply.github.com>
2024-05-05 10:18:12 -0600

    Update Program.cs

--- BulkyBook/Program.cs
+++ BulkyBook/Program.cs
@@ -21,6 +21,7 @@ namespace BulkyBook
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+
                 });
     }
 }
